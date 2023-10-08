7b2fe1d627fa4101a291b3c070bde2b3e65cba85
SkryabinGM <georgeskr94@gmail.com>
2023-10-08 17:45:27 +0300

    Сделал Валидатор

--- src/main/java/org/example/validator/Main.java
+++ src/main/java/org/example/validator/Main.java
@@ -3,13 +3,13 @@ package org.example.validator;
 public class Main {
     public static void main(String[] args) {
         StringDataValidator stringDataValidator = new StringDataValidator();
-        boolean test1 = stringDataValidator.validate("ABC", "ABC");
+/*        boolean test1 = stringDataValidator.validate("GAD", "GAD");
         boolean test2 = stringDataValidator.validate("ABC", "123");
-        boolean test3 = stringDataValidator.validate("Привет", "Привет");
-        boolean test4 = stringDataValidator.validate("", "привет");
-        System.out.println(test1);
+        boolean test3 = stringDataValidator.validate("Привет", "Привет");*/
+        boolean test4 = stringDataValidator.validate("123", "***");
+/*        System.out.println(test1);
         System.out.println(test2);
-        System.out.println(test3);
+        System.out.println(test3);*/
         System.out.println(test4);
     }
 }
--- src/main/java/org/example/validator/StringDataValidator.java
+++ src/main/java/org/example/validator/StringDataValidator.java
@@ -3,31 +3,45 @@ package org.example.validator;
 public class StringDataValidator {
     public boolean validate(String first, String second) throws DataException {
         String validCharacter ="ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-        if (!validCharacter.contains(String.valueOf(first))){
+
+        if (first==null) {
+            System.err.println("Сделано неправильно первое число null");
+            throw new EmptyDataException(second);
+        }
+        if (second==null) {
+            System.err.println("Сделано неправильно второе число null");
+            throw new EmptyDataException(second);
+        }
+        if (first.isEmpty()){
+            System.err.println("Сделано неправильно первое слово пустое");
+            throw new EmptyDataException(first);
+        }
+        if (second.isEmpty()){
+            System.err.println("Сделано неправильно второе слово пустое");
+            throw new EmptyDataException(second);
+        }
+
+
+/*        if (!validCharacter.contains(String.valueOf(first))){
             System.out.println("Сделано неправильно, в первом слове введены запрешенные символы");
             return false;
         }
         if (!validCharacter.contains(String.valueOf(second))){
             System.out.println("Сделано неправильно, во втором слове введены запрешенные символы");
             return false;
-        }
-
-        if (first.isEmpty()){
-            System.err.println("Сделано неправильно первое слово пустое");
-            return false;
-        }
-        if (second.isEmpty()){
-            System.err.println("Сделано неправильно второе слово пустое");
-            return false;
-        }
-/*        if (first==null) {
-            System.err.println("Сделано неправильно первое число null");
-            return false;
-        }
-        if (second==null) {
-            System.err.println("Сделано неправильно второе число null");
-            return false;
         }*/
+        for (int i = 0; i < first.length(); i++) {
+            if (!validCharacter.contains(String.valueOf(first.charAt(i)))){
+                System.out.println("Сделано неправильно, в первом слове введены запрешенные символы");
+                throw new UnknownCharacterException(first);
+            }
+        }
+        for (int i = 0; i < second.length(); i++) {
+            if (!validCharacter.contains(String.valueOf(second.charAt(i)))){
+                System.out.println("Сделано неправильно, во втором слове введены запрешенные символы");
+                throw new UnknownCharacterException(second);
+            }
+        }
         if (first.equals(second)) {
             return true;
         }
